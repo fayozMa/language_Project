8b4fe2f7c51ac8db885aa446c88ab12b5fdf8646
Fayyozbek <usmonovbahriddin108@gmail.com>
2024-10-16 22:46:26 +0500

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
+// react
+import React, { createContext, useState } from "react";
+//pages
+import Navbar from "./Components/Navbar";
+// context
+export const UserContext = createContext();
+//i18n
+import { useTranslation } from "react-i18next";
 function App() {
+  const { t, i18n } = useTranslation();
+  const [user, setUser] = useState({ theme: "light", lang: "eng" });
   return (
     <>
-      <Navbar/>
-      <div className="hero bg-[#EDF4FF] mt-6">
-        
-      </div>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Navbar />
+        <div className={`hero flex gap-[127px] rounded-[52px] max-w-[1388px] ${user.theme == 'light'?"bg-[#EDF4FF]" : "bg-[#161616]"} mx-auto mt-6 py-[100px] px-[130px]`}>
+          <div className="left max-w-[547px]">
+            <h4 className="font-bold text-lg leading-6 text-[#676767] mb-5">
+              {t("A SECURITY SOLUTION")}
+            </h4>
+            <h1 className={`font-black text-5xl leading-[62px] ${user.theme == 'light' ? "text-[#00085E]":"text-[#A1A9FF]"} mb-6`}>
+              {t("Keep track of your loved ones in real time!")}
+            </h1>
+            <div className=" border-t-2 border-[#D6D6D6] pt-4 flex gap-7 mb-16">
+              <img src="../public/assets/pin.svg" alt="pin" />
+              <p className={`font-medium text-xl leading-7 ${user.theme == 'light' ? "text-[#00085E]":"text-[#878787]"}`}>
+                {t(
+                  "We noticed the high level of insecurity in the world, and we thought of how technology could be used to combat these insecurities."
+                )}
+              </p>
+            </div>
+            <h3 className={`font-extrabold text-4xl leading-[56px] mb-3 ${user.theme == 'light' ? "text-black":"text-[#FFFFFF]"}`}>
+              {t("Join Our Waitlist!")}
+            </h3>
+            <p className={`font-medium text-xl leading-7 mb-8 ${user.theme == 'light' ? "text-[#00085E]":"text-[#878787]"}`}>
+              {t("Be the first to get notified when the product is ready!")}
+            </p>
+            <div className={`flex bg-white rounded-full shadow-md overflow-hidden max-w-[527px] ${user.theme == 'light' ? "bg-white":"bg-[#0d0d0d]"}`}>
+              <input
+                type="email"
+                placeholder={t("Your email address")}
+                className={`px-4 py-1 bg-white w-full rounded-full ${user.theme == 'light' ? "text-white":"bg-[#0d0d0d]"} outline-none`}
+              />
+              <button className="bg-[#0B56E8] text-white px-6 py-1 rounded-full font-bold">
+                {t("JOIN LIST!")}
+              </button>
+            </div>
+          </div>
+          <div className="right relative">
+            <img className=" ml-96" src="../public/assets/Path.svg" alt="path" />
+            <img src="../public/assets/phone.png" alt="phones" />
+            <img src="../public/assets/Path.svg" alt="path" />
+          </div>
+        </div>
+      </UserContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,39 +1,99 @@
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../App";
+import toast from "react-hot-toast";
+import { useTranslation } from "react-i18next";
+
 function Navbar() {
+  const { t, i18n } = useTranslation();
+  const { user, setUser } = useContext(UserContext);
+  const [lang, setLang] = useState("en");
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setUser({ ...user, theme: storedTheme });
+    }
+  }, []);
+
+  const handleTheme = () => {
+    const newTheme = user.theme === "light" ? "dark" : "light";
+    setUser({ ...user, theme: newTheme });
+    localStorage.setItem("theme", newTheme);
+    toast.success(t('Theme changed'));
+  };
+
+  const handleChangeLanguage = (e) => {
+    setLang(e.target.value);
+    toast.success('Language changed')
+  };
+
+  useEffect(() => {
+    i18n.changeLanguage(lang);
+  }, [lang]);
+
   return (
-    <div className="w-full shadow-[0px_1px_20px_0px_#0000001A]">
-      <div className="mx-auto max-w-[904px] py-8 flex items-center gap-20 px-4 ">
-      <a href="/" className="font-bold text-3xl leading-9 tracking-wide">
-        YOUR LOGO
-      </a>
-      <div className="flex gap-7">
-        <select>
-          <option disabled selected className="font-medium text-xl">
-            Assets
-          </option>
-          <option className="font-medium text-xl">option 1</option>
-          <option className="font-medium text-xl">option 2</option>
-          <option className="font-medium text-xl">option 3</option>
-        </select>
-        <a className="font-medium text-xl" href="">
-          Creators
+    <div
+      className={`w-full shadow-[0px_1px_20px_0px_#0000001A] ${
+        user.theme === "light"
+          ? "bg-white text-black"
+          : "bg-[#0D0D0D] text-white"
+      }`}
+    >
+      <div className="mx-auto max-w-[1200px] py-8 flex items-center gap-20 px-4">
+        <a href="/" className="font-bold text-3xl leading-9 tracking-wide">
+          {t("YOUR LOGO")}
         </a>
-        <a className="font-medium text-xl" href="">
-          Careers
-        </a>
-        <a className="font-medium text-xl" href="">
-          Go Pro
-        </a>
+        <div className="flex gap-7">
+          <select
+            className={`rounded ${
+              user.theme === "light"
+                ? "bg-white text-black"
+                : "bg-[#0D0D0D] text-white"
+            }`}
+          >
+            <option disabled selected className="font-medium text-xl">
+              {t('Assets')}
+            </option>
+            <option className="font-medium text-xl">option 1</option>
+            <option className="font-medium text-xl">option 2</option>
+            <option className="font-medium text-xl">option 3</option>
+          </select>
+          <a className="font-medium text-xl" href="">
+            {t('Creators')}
+          </a>
+          <a className="font-medium text-xl" href="">
+            {t('Careers')}
+          </a>
+          <a className="font-medium text-xl" href="">
+            {t('Go Pro')}
+          </a>
+        </div>
+        <div className="flex gap-5">
+          <select
+            onChange={handleChangeLanguage}
+            value={lang}
+            className={`rounded ${
+              user.theme === "light"
+                ? "bg-white text-black"
+                : "bg-[#0D0D0D] text-white"
+            }`}
+          >
+            <option className="font-medium text-xl" disabled selected>
+              Language
+            </option>
+            <option value="uz" className="font-medium text-xl">
+              O'zb
+            </option>
+            <option value="ru" className="font-medium text-xl">
+              Рус
+            </option>
+            <option value="en" className="font-medium text-xl">
+              Eng
+            </option>
+          </select>
+          <button onClick={handleTheme}>{t('Mode')}</button>
+        </div>
       </div>
-      <div className="flex gap-5">
-        <select className="p-2">
-          <option className="font-medium text-xl" disabled selected>Language</option>
-            <option className="font-medium text-xl" >O'zb</option>
-            <option className="font-medium text-xl" >Рус</option>
-            <option className="font-medium text-xl" >Eng</option>
-        </select>
-        <button>Dark</button> 
-      </div>
-    </div>
     </div>
   );
 }
